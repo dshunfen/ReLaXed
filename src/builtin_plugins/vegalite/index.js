2b452cf1f4615dd20eeb13a4ee0a025a9f71ebc3
David Shunfenthal <dshunfenthal@cyberpointllc.com>
2019-09-20 16:50:45 -0400

    Adding in basic functionality to generate the SVG from JSON provided in the pug

--- src/builtin_plugins/vegalite/index.js
+++ src/builtin_plugins/vegalite/index.js
@@ -1,8 +1,8 @@
 const pug = require('pug')
 const fs = require('fs')
 const path = require('path')
-const vegaembed = require('vega-embed')
 
+const VEGA_EXT = '.vegalite.json'
 const RENDER_EXT = '.svg'
 
 exports.constructor = async function (params) {
@@ -16,28 +16,27 @@ exports.constructor = async function (params) {
     ],
     pugFilters: {
       vegaLite (text, options) {
-        return vegaembed('#vis', text, {'renderer': 'svg'});
+        return vegaHtml(text);
       }
     }
   }
 }
 
+function vegaHtml(vegaliteSpec) {
+  return pug.renderFile(path.join(__dirname, 'template.pug'), {vegaliteSpec});
+}
+
 var vegaliteHandler = async function (vegalitePath, page) {
   var vegaliteSpec = fs.readFileSync(vegalitePath, 'utf8')
-  var html = pug.renderFile(path.join(__dirname, 'template.pug'), { vegaliteSpec })
+  var html = vegaHtml(vegaliteSpec)
 
   await page.setContent(html)
-  await page.waitForSelector('#vis svg')
+  await page.waitForSelector('#vis')
 
   var svg = await page.evaluate(function () {
-    var el = document.querySelector('#vis svg')
-
-    el.removeAttribute('height')
-    el.removeAttribute('width')
-
-    return el.outerHTML
+      return document.querySelector('#vis').outerHTML;
   })
 
-  var svgPath = vegalitePath.substr(0, vegalitePath.length - '.vegalite.json'.length) + RENDER_EXT
+  var svgPath = vegalitePath.substr(0, vegalitePath.length - VEGA_EXT.length) + RENDER_EXT
   fs.writeFileSync(svgPath, svg)
 }
